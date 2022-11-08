f103087410cf5ee608d42fabf6fadaa5c489e82b
Arman-Hossen <arman15-12726@diu.edu.bd>
2022-11-08 22:33:41 +0600

    service details page and reveiw section

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,28 +1,74 @@
 import React, { useContext } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Contex/AuthProvider/AuthProvider";
-import { ImageViewer } from "react-image-viewer-dv"
+import { ImageViewer } from "react-image-viewer-dv";
+import { BsFillPeopleFill } from "react-icons/bs";
 
 const ServiceDetails = () => {
-  const { _id,img, title, price, service_id, rating, total_reveiwer, description } =
-    useLoaderData();
+  const {
+    _id,
+    img,
+    title,
+    price,
+    service_id,
+    rating,
+    total_reveiwer,
+    description,
+  } = useLoaderData();
   const { user } = useContext(AuthContext);
   return (
     <div>
-      <h1 className="text-center">You are Clicking : {title}</h1>
+      <h1 className="text-center">Details And Reveiw </h1>
       <div className="grid grid-cols-1 m-4">
-      <div className="card card-compact  bg-base-100 shadow-xl mb-4 text-center">
-      <h2 className=" text-2xl font-bold text-center">{title}</h2>
-  <figure><ImageViewer><img className='h-60' src={img} alt="" /></ImageViewer></figure>
-  <div className="card-body">
-    
-    <p className='text-xl font-semibold text-orange-600'>Price: {price}</p>
-    <p className='text-xl font-semibold text-orange-600'>Rating: {rating}</p>
-    <p className=''> {description.slice(0,100)+'...'}</p>
-   
-  </div>
-</div>
-        <div></div>
+        <h2 className="text-center font-bold text-2xl text-orange-500 m-5">
+          {" "}
+          Service Details
+        </h2>
+        <div className="card card-compact  bg-base-100 shadow-xl mb-4 text-center">
+          <figure>
+            <ImageViewer>
+              <img className="rounded-xl w-full" src={img} alt="" />
+            </ImageViewer>
+          </figure>
+          <h2 className=" text-2xl font-bold text-center m-4 ">{title}</h2>
+          <div className="card-body ">
+            <p className="text-xl font-semibold text-orange-600">
+              Product No: {service_id}
+            </p>
+            <p className="text-xl font-semibold text-orange-600">
+              Price: {price}
+            </p>
+            <p className="text-xl font-semibold text-orange-600">
+              Rating: {rating}
+            </p>
+          </div>
+
+          <p className=""> {description}</p>
+          <p className="text-xl font-semibold text-orange-600 m-4">
+            {" "}
+            Reveiw Given By {total_reveiwer} people
+          </p>
+        </div>
+        <div>
+          <h2 className="text-center font-bold text-2xl text-orange-500 m-5">
+            {" "}
+            Reveiw
+          </h2>
+          {
+             user?.email?
+             <>
+             <div className="text-center">
+             <Link to='/'><button className="btn btn-primary">Give Reviw</button></Link>
+             </div>
+             </>
+             :
+             <>
+             <div className="text-center">
+             <Link to='/login'><button className="btn btn-primary">Log in for Give Reveiw</button></Link>
+             </div>
+             </>
+          }
+        </div>
       </div>
     </div>
   );
