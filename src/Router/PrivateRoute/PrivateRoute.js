dc055ddcf69dd9011c2720941e2f42eddb8739ab
Arman-Hossen <arman15-12726@diu.edu.bd>
2022-11-09 10:33:00 +0600

    Private Route Error fixed

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -2,19 +2,14 @@ import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../../Contex/AuthProvider/AuthProvider";
 
-const PrivateRoute = ({ childred }) => {
+const PrivateRoute = ({children}) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
   if (loading) {
-    return <button type="button" class="bg-indigo-500 ..." disabled>
-    <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
-      
-    </svg>
-    Processing...
-  </button>;
+    return <h1 className="text-5xl">Loading...</h1>;
   }
   if (user) {
-    return childred;
+    return children;
   }
   return (
     <div>
